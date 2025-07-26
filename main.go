6204b9288a2071394cf9ffd4189d91bdc9c21c43
K <kareem.m.dev@gmail.com>
2025-07-26 11:56:07 +0000

    First prototype

--- main.go
+++ main.go
@@ -23,9 +23,15 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
+		fmt.Print("> ")
 		input, err := reader.ReadString('\n')
 
 		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		if err := execInput(input); err != nil {
+
 			fmt.Fprintln(os.Stderr, err)
 		}
 	}
