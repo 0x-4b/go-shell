37baa1d5ba48efe3b4f20a3fd8c657fde1c36931
K <kareem.m.dev@gmail.com>
2025-07-26 11:52:08 +0000

    Setup execution logic

--- main.go
+++ main.go
@@ -4,13 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
+
+func execInput(input string) error {
+	input = strings.TrimSuffix(input, "\n")
+
+	cmd := exec.Command(input)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stderr = os.Stdout
+
+	return cmd.Run()
+}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		input, err := reader.ReadString("/n")
+		input, err := reader.ReadString("\n")
 
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
