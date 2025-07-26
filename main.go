38487c2626f65419232d057c7db97c202f0d1b8a
K <kareem.m.dev@gmail.com>
2025-07-26 12:07:26 +0000

    Rearrangement

--- main.go
+++ main.go
@@ -9,6 +9,26 @@ import (
 	"strings"
 )
 
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		input, err := reader.ReadString('\n')
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		if err := execInput(input); err != nil {
+
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+
+var ErrNoPath = errors.New("path required")
+
 func execInput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
 
@@ -33,23 +53,3 @@ func execInput(input string) error {
 
 	return cmd.Run()
 }
-
-func main() {
-	reader := bufio.NewReader(os.Stdin)
-
-	for {
-		fmt.Print("> ")
-		input, err := reader.ReadString('\n')
-
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-
-		if err := execInput(input); err != nil {
-
-			fmt.Fprintln(os.Stderr, err)
-		}
-	}
-}
-
-var ErrNoPath = errors.New("path required")
